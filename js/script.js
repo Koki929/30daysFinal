eb938abc24649ac899530434479254fb03180dc7
Koki929 <2013edcckokimatsushita@gmail.com>
2020-02-13 15:13:24 +0900

    編集2/13

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-// drawer
-$(document).ready(function() {
-    $('.drawer').drawer();
-});
-$(function() {
-    $('.hamburger, .drawer-overlay, .drawer-menu-item').click(function() {
-      $('.bar1, .bar2, .bar3, .header-section').toggleClass('open');
-    })
+// hiraku
+$(".offcanvas-right").hiraku({
+    btn:".offcanvas-btn-right",
+    direction:"right"
 });
