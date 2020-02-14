58e98d0a41783043228a3fc16b00f203f1071553
Koki929 <2013edcckokimatsushita@gmail.com>
2020-02-14 22:05:09 +0900

    編集2 2/14

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ $(".offcanvas-right").hiraku({
     btn:".offcanvas-btn-right",
     direction:"right"
 });
+
+//slider
+$('.results-sliders').slick({
+    arrows: false,
+    infinite: true, //スライドのループ有効化
+    dots:true, //ドットのナビゲーションを表示
+    centerMode: true, //要素を中央寄せ
+    centerPadding:'350px', //両サイドの見えている部分のサイズ
+    autoplay:true, //自動再生
+});
+
+// Wow
+new WOW().init();
