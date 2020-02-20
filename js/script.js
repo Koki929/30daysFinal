0252db503350793423200f464e7e63e32142298c
Koki929 <2013edcckokimatsushita@gmail.com>
2020-02-20 16:09:45 +0900

    編集2/20

--- js/script.js
+++ js/script.js
@@ -4,6 +4,39 @@ $(".offcanvas-right").hiraku({
     direction:"right"
 });
 
+$('a[href^="#"]').click(function() {
+
+    // 画面のロックを解除する
+    $('body').css('position', 'static');
+    $('body').css('overflow', 'scroll');
+    $('body').css('overflow-y', 'scroll');
+
+    // メニューを閉じる
+    $('.js-hiraku-offcanvas-open').hide();
+    $('.js-hiraku-offcanvas-body-right').removeAttr('style');
+
+    // ヘッダーの表示を切り替える
+    $('header').removeAttr('style');
+    $('.hiraku-open-btn').removeAttr('style');
+    $('.open-btn').show();
+    $('.close-btn').hide();
+
+    // classを初期化する
+    $('body').removeClass('js-hiraku-offcanvas-body-right');
+    $('.hiraku-open-btn').removeClass('js-hiraku-offcanvas-btn-active');
+    $('.hiraku-open-btn').attr({'aria-expanded' : 'false'});
+    $('.js-hiraku-offcanvas').removeClass('js-hiraku-offcanvas-open');
+    $('.js-hiraku-offcanvas-sidebar').removeClass('active');
+
+    // styleを初期化
+    $('html').removeAttr('style');
+    $('body').removeAttr('style');
+    $('header').removeAttr('style');
+    // $('.hiraku-open-btn').removeAttr('style');
+    $('.close-btn').removeAttr('style');
+    $('.js-hiraku-offcanvas').removeAttr('style');
+  });
+
 //slider
 $('.results-sliders').slick({
     arrows: false,
