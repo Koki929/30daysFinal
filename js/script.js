27efe870ecc7863440fef780707b204af8806b0e
Koki929 <2013edcckokimatsushita@gmail.com>
2020-02-23 15:16:59 +0900

    編集2/23

--- js/script.js
+++ js/script.js
@@ -42,9 +42,8 @@ $('.results-sliders').slick({
     arrows: false,
     infinite: true, //スライドのループ有効化
     dots:true, //ドットのナビゲーションを表示
-    centerMode: true, //要素を中央寄せ
-    centerPadding:'350px', //両サイドの見えている部分のサイズ
     autoplay:true, //自動再生
+    slidesToShow: 3
 });
 
 // Wow
